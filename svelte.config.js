0e9871ca3b66bb8a288ed00254be4f89060e2eb8
OwLu0905 <owlu0905@gmail.com>
2025-03-26 00:54:39 +0800

    chore: deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +6,21 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		paths: {
+			base: "/memowild",
+		},
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// If the error is about a path not starting with the base path, ignore it
+				if (message.includes("does not begin with `base`")) {
+					return;
+				}
+				// Otherwise, throw the error
+				throw new Error(message);
+			},
+		},
+	},
 };
 
 export default config;
